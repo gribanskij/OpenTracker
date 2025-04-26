80e42d8d08e0cbfb4a643be09b7e189670cbb41f
Dmitriy Gribanskij <gribanskij1979@gmail.com>
2025-04-26 20:35:48 +0300

    some tests 2

--- app/src/main/java/com/gribansky/opentracker/core/TrackerService.kt
+++ app/src/main/java/com/gribansky/opentracker/core/TrackerService.kt
@@ -1,15 +1,15 @@
 package com.gribansky.opentracker.core
 
 import android.app.AlarmManager
+import android.app.PendingIntent
 import android.app.Service
-import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
 import android.os.Binder
-import android.os.Build
 import android.os.IBinder
 import android.os.PowerManager
 import android.os.PowerManager.WakeLock
+import android.os.SystemClock
 import android.util.Log
 import androidx.preference.PreferenceManager
 import kotlinx.coroutines.Job
@@ -23,6 +23,8 @@ import kotlin.random.nextInt
 
 private const val WAKE_LOCK_NAME = "OpenTracker:TrackerService"
 private const val TAG = "TrackerService"
+private const val TRACK_TIMER = 100
+private const val TRACK_TIMER_ACTION = "intent.action.TIMER_FIRED"
 
 
 class TrackerService : Service() {
@@ -59,6 +61,9 @@ class TrackerService : Service() {
         super.onCreate()
         lock.acquire()
         Log.d(TAG,"onCreate...")
+
+
+
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
@@ -114,6 +119,17 @@ class TrackerService : Service() {
         Log.d(TAG,"setUpClient...")
     }
 
+
+    private fun restartTimer(triggerTime:Long, interval:Long){
+        val intent  = Intent(this, TrackerReceiver::class.java).apply {
+            action = TRACK_TIMER_ACTION
+        }
+        val pIntent = PendingIntent.getBroadcast(this, TRACK_TIMER, intent, PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE)
+
+        (getSystemService(ALARM_SERVICE) as AlarmManager).apply {
+            setRepeating(AlarmManager.ELAPSED_REALTIME_WAKEUP, triggerTime, interval, pIntent)
+        }
+    }
 
     inner class LocalBinder : Binder() {
         fun getService(): TrackerService = this@TrackerService
