c95f415970a44d0cfc680e3ff2446fcf7cedd020
diugribanskii <santygo@tut.by>
2025-05-27 23:05:25 +0300

    DEV:  compose implementing

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.serialization.plugin)
+    alias(libs.plugins.compose.plugin)
 }
 
 android {
@@ -63,8 +64,19 @@ dependencies {
     androidTestImplementation(composeBom)
     implementation(libs.compose.navigation)
 
+    implementation("androidx.compose.foundation:foundation")
+    implementation ("androidx.compose.runtime:runtime")
+    implementation ("androidx.compose.ui:ui")
+    implementation ("androidx.compose.foundation:foundation")
+    implementation ("androidx.compose.material:material")
+    implementation ("androidx.compose.material:material-icons-extended")
+    implementation ("androidx.activity:activity-compose:1.10.1")
+    implementation ("androidx.navigation:navigation-compose:2.9.0")
+    debugImplementation ("androidx.compose.ui:ui-tooling")
 
 
+    testImplementation("androidx.compose.ui:ui-test-junit4")
+    androidTestImplementation("androidx.compose.ui:ui-test")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- build.gradle.kts
+++ build.gradle.kts
@@ -3,4 +3,5 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.serialization.plugin) apply false
+    alias(libs.plugins.compose.plugin) apply false
 }
